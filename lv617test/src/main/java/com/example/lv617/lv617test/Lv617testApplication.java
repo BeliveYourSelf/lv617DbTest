e9a639bd1569999e1010dcb554e83e51d78bce99
BeliveYourSelf <454886565@qq.com>
2020-05-18 17:56:17 +0800

    Initial commit

---
+++ lv617test/src/main/java/com/example/lv617/lv617test/Lv617testApplication.java
@@ -0,0 +1,12 @@
+package com.example.lv617.lv617test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lv617testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lv617testApplication.class, args);
+	}
+}
